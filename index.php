54391684f4a1366d619b6668970ffd62c7a63cf4
delimaaa <131340266+delimaaa@users.noreply.github.com>
2023-04-20 13:58:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,133 @@
+    <!doctype html>
+    <html class="no-js" lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+        <title>SIAD | Sistem Informasi Arsip Digital </title>
+        <link rel="stylesheet" type="text/css" href="asset/css/css.css">
+        <link id="favicon" rel="shortcut icon" type="image/png" href="unima.png">
+
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    </head>
+
+    <body>
+
+        <style type="text/css">
+
+            .navbar-siad{
+                background: #ffa500;
+                border-radius: 0px;
+                border: 1px solid  #ffa500;
+                margin: 0px;
+                padding: 20px 0px;
+            }
+
+            .navbar-inverse .navbar-brand {
+
+                color: #fff;
+
+            }
+
+            .navbar-inverse .navbar-nav > li > a {
+
+                color: #fff;
+
+            }
+            .navbar-siad > li > a {
+
+                color: #9d9d9d !important;
+
+            }
+
+            .banner{
+                background:     #ffa500;
+                border-radius: 0px;
+                border: 1px solid  #ffa500;
+                padding: 60px 0px;
+            }
+
+            .banner{
+                color: white;
+            }
+
+            .banner a{
+                padding: 15px 25px;
+                color: white;
+                border: 1px solid white;
+                -webkit-transition: all 0.5s; 
+                transition: all 0.5s;
+                margin-right: 10px;
+            }
+
+            .banner a:hover{
+                text-decoration: none;
+                border: 1px dashed white;
+            }
+
+            .banner p{
+                font-size: 13pt;
+            }
+        </style>
+
+
+
+        <nav class="navbar navbar-inverse navbar-siad">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <img src="tutwurii.png" width="105" height="110">
+                    <img src="unima.png" width="105" height="110">               
+                    
+                    
+
+                    <h2 style="color:#FFFFFF;"><b>JURUSAN PENDIDIKAN TEKNOLOGI INFORMASI DAN KOMUNIKASI <br>
+                    BIDANG ADMINISTRASI</b><br>
+                    Sistem Informasi Arsip Digital</a><br>                   
+                    </h2></div></div></nav>
+                    
+     
+
+        <div class="banner">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div style="margin-top: 140px;">
+                            <h1>Sistem Informasi Arsip Digital</h1>
+                            <p>Pengarsipan Surat Masuk dan Surat Keluar Administrasi PTIK</p>
+
+                            <br>
+                            <br>
+
+                            <a href="user_login.php">LOGIN USER</a>
+                            <a href="login.php">LOGIN ADMIN</a>
+
+                            
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                    <img src="arsip1.png" width="650" height="440">
+                        
+                    </div>
+                </div>
+        <marquee>Sistem Informasi Arsip Digital (Pengarsipan Data lebih Cepat dan Aman) - JURUSAN PENDIDIKAN TEKNOLOGI INFORMASI DAN KOMUNIKASI</marquee>
+            </div>
+        </div>
+
+        <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+    </body>
+
+    </html>
